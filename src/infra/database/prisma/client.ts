d7790b39fa8700f0002ee4f583bc3b01d6cd22b1
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-27 16:10:43 -0300

    feat(main/factory): add usecases factories - FetchTicketsUseCase - GetTicketUseCase

--- src/infra/database/prisma/client.ts
+++ src/infra/database/prisma/client.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
+// Re-using a single PrismaClient instance
+// https://www.prisma.io/docs/guides/performance-and-optimization/connection-management#prismaclient-in-long-running-applications
 let prisma = new PrismaClient()
 
 export default prisma
---
+++ src/main/factories/usecases/index.ts
@@ -0,0 +1,2 @@
+export * from '@/main/factories/usecases/fetch-tickets-factory'
+export * from '@/main/factories/usecases/get-ticket-factory'
