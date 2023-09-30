a50848e342991d9c11bf985a1e1b1706045a8bc1
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-30 13:59:41 -0300

    feat(infra/database): add pagination in PrismaTicketRepository

--- src/infra/database/prisma/repositories/prisma-ticket-repository.ts
+++ src/infra/database/prisma/repositories/prisma-ticket-repository.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from '@prisma/client'
-import { TicketRepository } from '@/application/protocols'
+import { PaginatedData, RepositoryParams, TicketRepository } from '@/application/protocols'
 import { Ticket } from '@/domain/entities'
 import { PrismaTicketMapper } from '@/infra/database/prisma/mappers/prisma-ticket-mapper'
+import { PaginationHelper } from '@/utils/pagination'
 
 export class PrismaTicketRepository implements TicketRepository {
-  constructor(private prisma: PrismaClient) {}
+  constructor(private prisma: PrismaClient) {
+  }
 
   async findById(id: string): Promise<Ticket | null> {
     const ticket = await this.prisma.ticket.findUnique({ where: { id } })
@@ -16,8 +18,20 @@ export class PrismaTicketRepository implements TicketRepository {
     return PrismaTicketMapper.toDomain(ticket)
   }
 
-  async findMany(): Promise<Ticket[]> {
-    const tickets = await this.prisma.ticket.findMany()
-    return tickets.map(PrismaTicketMapper.toDomain)
+  async findMany({ pagination: { page, perPage } }: RepositoryParams): Promise<PaginatedData<Ticket[]>> {
+    const count = await this.prisma.ticket.count()
+
+    const pagination = PaginationHelper.create({
+      page,
+      perPage,
+      totalItems: count
+    })
+
+    const tickets = await this.prisma.ticket.findMany({
+      skip: pagination.offset,
+      take: pagination.perPage
+    })
+
+    return { data: tickets.map(PrismaTicketMapper.toDomain), pagination }
   }
 }
