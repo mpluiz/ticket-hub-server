e73f46b9004ed1bee2174683784e9c488fc4ea19
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-09 00:07:22 -0300

    feat(application/protocol): add methods in CartItemRepository - findAlreadyExistsCartItem - update

--- src/application/protocols/repositories/cart-item-repository.ts
+++ src/application/protocols/repositories/cart-item-repository.ts
@@ -3,8 +3,10 @@ import { CartItem } from '@/domain/entities'
 export abstract class CartItemRepository {
   abstract findById(id: string): Promise<CartItem | null>
   abstract findMany(): Promise<CartItem[]>
+  abstract findAlreadyExistsCartItem(cartId:string, ticketId: string): Promise<CartItem | null>
   abstract findManyByTicketId(ticketId: string): Promise<CartItem[]>
   abstract findManyByCartId(cartId: string): Promise<CartItem[]>
   abstract create(cartItem: CartItem): Promise<void>
+  abstract update(cartItem: CartItem): Promise<void>
   abstract delete(id: string): Promise<void>
 }
