6f76e79136d58fca43b81ebbd9acb192f85f533e
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-09 00:09:02 -0300

    feat(application/domain): add cart item already exists condition on create cart item

--- src/application/usecases/create-cart-item.ts
+++ src/application/usecases/create-cart-item.ts
@@ -2,16 +2,26 @@ import { Either, success } from '@/utils/either'
 import { CartItemRepository } from '@/application/protocols'
 import { CartItem, UniqueEntityID } from '@/domain/entities'
 
-type CreateCartItemUseCaseRequest = { cartId: string, ticketId: string, quantity: number }
+type CreateCartItemUseCaseRequest = { cartId: string, ticketId: string, subTotal: number, quantity: number }
 type CreateCartItemUseCaseResponse = Either<null, { cartItem: CartItem }>
 
 export class CreateCartItemUseCase {
   constructor(private cartItemRepository: CartItemRepository) {}
 
-  async execute({ cartId, ticketId, quantity }: CreateCartItemUseCaseRequest): Promise<CreateCartItemUseCaseResponse> {
+  async execute({ cartId, ticketId, quantity, subTotal }: CreateCartItemUseCaseRequest): Promise<CreateCartItemUseCaseResponse> {
+    const alreadyExistsCartItem = await this.cartItemRepository.findAlreadyExistsCartItem(cartId, ticketId)
+
+    if (alreadyExistsCartItem) {
+      alreadyExistsCartItem.quantity = alreadyExistsCartItem.quantity + quantity
+      await this.cartItemRepository.update(alreadyExistsCartItem)
+
+      return success({ cartItem: alreadyExistsCartItem })
+    }
+
     const cartItem = CartItem.create({
       cartId: new UniqueEntityID(cartId),
       ticketId: new UniqueEntityID(ticketId),
+      subTotal,
       quantity,
       createdAt: new Date()
     })
