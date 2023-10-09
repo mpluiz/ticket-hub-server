1127d5345e66084c32d388b172806f78aea8048c
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-09 00:04:14 -0300

    feat(application/protocol): add update method in CartRepository

--- src/application/protocols/repositories/cart-repository.ts
+++ src/application/protocols/repositories/cart-repository.ts
@@ -4,4 +4,5 @@ export abstract class CartRepository {
   abstract findById(id: string): Promise<Cart | null>
   abstract findMany(): Promise<Cart[]>
   abstract create(cart: Cart): Promise<void>
+  abstract update(cart: Cart): Promise<void>
 }
