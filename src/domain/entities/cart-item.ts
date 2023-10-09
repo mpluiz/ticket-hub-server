0892b99235d934bdd39000c1820421a807df5bab
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-09 00:06:17 -0300

    feat(domain/entity): add subTotal attribute in CartItem

--- src/domain/entities/cart-item.ts
+++ src/domain/entities/cart-item.ts
@@ -3,6 +3,7 @@ import { Entity, UniqueEntityID } from '@/domain/entities'
 export interface CartItemProps {
   cartId: UniqueEntityID
   ticketId: UniqueEntityID
+  subTotal: number
   quantity: number
   createdAt: Date
 }
@@ -18,5 +19,17 @@ export class CartItem extends Entity<CartItemProps> {
 
   get ticketId(): UniqueEntityID {
     return this.props.ticketId
+  }
+
+  get subTotal(): number {
+    return this.props.subTotal
+  }
+
+  get quantity(): number {
+    return this.props.quantity
+  }
+
+  set quantity(value: number) {
+    this.props.quantity = value
   }
 }
