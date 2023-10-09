392c23a5396ae97d456095ddb08614f5d0081984
mpluiz <marcospaulo.luiz@gmail.com>
2023-10-09 00:05:12 -0300

    feat(domain/entity): add getters and setters in Cart - subTotal - total

--- src/domain/entities/cart.ts
+++ src/domain/entities/cart.ts
@@ -11,4 +11,20 @@ export class Cart extends Entity<CartProps> {
   static create(props: CartProps, id?: UniqueEntityID): Cart {
     return new Cart(props, id)
   }
+
+  get subTotal(): number {
+    return this.props.subTotal
+  }
+
+  set subTotal(value: number) {
+    this.props.subTotal = value
+  }
+
+  get total(): number {
+    return this.props.total
+  }
+
+  set total(value: number) {
+    this.props.total = value
+  }
 }
